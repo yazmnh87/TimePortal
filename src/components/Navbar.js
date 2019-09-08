2e50d94a32c59c3d7035cdc5da71fe71374d7624
William Violette <violettewj@gmail.com>
2019-09-08 17:37:03 -0600

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,20 @@ export const NavbarStyled = style.div`
 padding: 0px;
 margin: 0px;
 display: flex;
+flex-direction: row;
 background-color: ${alternate};
 width: 100%;
 position: fixed;
 z-index: 999;
 top: 0;
 height: 100px;
+justify-content: space-between;
 `
 
 const Logo = style.div`
 margin-left: 10px;
 align-self: center;
-font-size: 20px;
+font-size: 30px;
 text-shadow: 1px 1px 4px #380502;
 `
 const MenuContainer = style.div`
@@ -26,7 +28,7 @@ const MenuContainer = style.div`
 
 const MenuItems = style.div`
 display: flex;
-align-self: flex-end;
+align-self:flex-end;
 width: 200px;
 height: 100px;
 border: 1px solid black;
@@ -37,6 +39,8 @@ export const Navbar= () =>{
                 WildCard Dev
             </Logo>
             <MenuItems>
+            <div style={{alignSelf:'center', marginRight:'2%'}}><a style={{fontSize:22}}>Forms</a></div>
+            <div style={{alignSelf:'center'}}><a style={{fontSize:22}}>Account</a></div>
             </MenuItems>
     </NavbarStyled>
 }
